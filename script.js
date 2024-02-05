83a1981f4cbce53dbdb8aa3c523d9a1088f2f063
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-02-05 04:53:34 +0200

    a

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-const cells = document.querySelectorAll('.cell');
+const root = document.documentElement;
+const board = document.querySelector('.board')
+let cells = board.querySelectorAll('.cell');
 const reset = document.querySelector('#reset')
+const easy = document.querySelector('#easy')
+const medium = document.querySelector('#medium')
+const hard = document.querySelector('#hard')
+
+const createBoard = (numberOfCells) => {
+    board.innerHTML = ''; // Clear the board
+
+    for (let i = 0; i < numberOfCells; i++) {
+        const newCell = document.createElement('div');
+        newCell.classList.add('cell');
+
+        const spanValue = document.createElement('span');
+        spanValue.classList.add('value');
+
+        newCell.appendChild(spanValue);
+        board.appendChild(newCell);
+    }
+
+    cells = board.querySelectorAll('.cell');
+}
+
 
 const resetFunc = ()=>{
     cells.forEach((cell)=>{
@@ -11,15 +34,15 @@ const resetFunc = ()=>{
     });
 }
 
-const checkWinner = (char1, char2, char3, char4=0 , char5=0)=>{
-                if(char4 == char5 && char4 == 0){
+const checkWinner = (length, char1, char2, char3, char4='' , char5='')=>{
+                if(length == 9){
                     if(char1 == char2 && char2 == char3){
                         if(char1 === 'X'){
                                 setTimeout(() => {alert('Congrats, Player X Wins');resetFunc(); }, 300); }
                         else if(char1 === 'O'){setTimeout(() => {alert('Congrats, Player O Wins');resetFunc(); }, 300); }        
                     }
                 }
-                else if(char5 == 0){
+                else if(length == 16){
                     if(char1 == char2 && char2 == char3 && char3 == char4){
                         if(char1 === 'X'){
                                 setTimeout(() => {alert('Congrats, Player X Wins');resetFunc(); }, 300); }
@@ -35,7 +58,25 @@ const checkWinner = (char1, char2, char3, char4=0 , char5=0)=>{
                 }
 }
 
+easy.addEventListener('click',() => {
+    root.style.setProperty('--grid-rows','3');
+    createBoard(9);
+    // cells = board.querySelectorAll('.cell');
+});
+medium.addEventListener('click',() => {
+    root.style.setProperty('--grid-rows','4');
+    createBoard(16);
+    // cells = board.querySelectorAll('.cell');
+});
+hard.addEventListener('click',() => {
+    root.style.setProperty('--grid-rows','5');
+    createBoard(25);
+    // cells = board.querySelectorAll('.cell');
+});
+
+
 let count = 1 ; 
+// cells = board.querySelectorAll('.cell');
 cells.forEach((cell)=>{
     cell.addEventListener('click',()=>{
         if(!cell.classList.contains('cell--X') && !cell.classList.contains('cell--O') ){
@@ -65,15 +106,19 @@ cell.addEventListener('click',()=>{
     let char1 = cells[0].childNodes[1].innerText;
     let char2 = cells[(cells.length)**0.5 +1].childNodes[1].innerText;
     let char3 = cells[((cells.length)**0.5 +1)*2].childNodes[1].innerText;
-    let char4 = 0;
-    let char5 = 0 ;
-    if(cells.length>9){
+    let char4;
+    let char5;
+    if(cells.length<=9){
+        checkWinner(cells.length,char1, char2, char3)
+    }
+    else if(cells.length<=16){
         char4 = cells[((cells.length)**0.5 +1)*3].childNodes[1].innerText;
+        checkWinner(cells.length,char1, char2, char3, char4);
     }
-    if(cells.length>16){
+    else if(cells.length>16){
         char5 = cells[((cells.length)**0.5 +1)*4].childNodes[1].innerText;
+        checkWinner(cells.length,char1, char2, char3, char4 , char5);
     }
-    checkWinner(char1, char2, char3, char4 , char5)
     // =======================================================================================================================================================
     // ======================================================================== SECOND / line ================================================================
     // =======================================================================================================================================================
@@ -86,10 +131,10 @@ cell.addEventListener('click',()=>{
     if(cells.length>16){
         char5 = cells[((cells.length)**0.5 - 1)*5 ].childNodes[1].innerText;
     }            
-    checkWinner(char1, char2, char3, char4 , char5)
+    checkWinner(cells.length,char1, char2, char3, char4 , char5)
     
     
-    for (let i = 0; i < cells.length - (cells.length)**0.5; i++) {
+    for (let i = 0; i < cells.length - (cells.length)**0.5+1; i++) {
         // =======================================================================================================================================================
         // ====================================================================== Horizontal Line ================================================================
         // =======================================================================================================================================================
@@ -104,22 +149,22 @@ cell.addEventListener('click',()=>{
                 char5 = cells[i + 4].childNodes[1].innerText;
             }
         if(i===0 || i=== (cells.length)**0.5 || i===((cells.length)**0.5)*2){
-            checkWinner(char1, char2, char3, char4 , char5)
+            checkWinner(cells.length,char1, char2, char3, char4 , char5)
                 }
-        else if(cells.length>9 && i===((cells.length)**0.5)*3){
-            checkWinner(char1, char2, char3, char4 , char5)
+         if(cells.length>9 && i===((cells.length)**0.5)*3){
+            checkWinner(cells.length,char1, char2, char3, char4 , char5)
                 }
-        else if(cells.length>16 && i===((cells.length)**0.5)*4){
-            checkWinner(char1, char2, char3, char4 , char5)
+         if(cells.length>16 && i===((cells.length)**0.5)*4){
+            checkWinner(cells.length,char1, char2, char3, char4 , char5)
                 }
             }       
             // =======================================================================================================================================================
             // ====================================================================== Vertical Line ================================================================
             // =======================================================================================================================================================
-            for (let i = 0; i < cells.length - (cells.length)**0.5; i++) {
+            for (let i = 0; i < cells.length - (cells.length)**0.5 +1; i++) {
                 char1 = cells[i].childNodes[1].innerText;
                 char2 = cells[i + (cells.length)**0.5].childNodes[1].innerText;
-                char3 = cells[i + f((cells.length)**0.5)*2].childNodes[1].innerText;
+                char3 = cells[i + ((cells.length)**0.5)*2].childNodes[1].innerText;
                 if(cells.length>9){
                     char4 = cells[i + ((cells.length)**0.5)*3].childNodes[1].innerText;
                 }
@@ -128,13 +173,13 @@ cell.addEventListener('click',()=>{
                 }
                 
                 if(i===0 ||i===1 || i===2){
-                    checkWinner(char1, char2, char3, char4 , char5)
+                    checkWinner(cells.length,char1, char2, char3, char4 , char5)
                 }
-                else if(cells.length>9 && i=== 3){
-                    checkWinner(char1, char2, char3, char4 , char5)
+                if(cells.length>9 && i=== 3){
+                    checkWinner(cells.length,char1, char2, char3, char4 , char5)
                         }
-                else if(cells.length>16 && i=== 4){
-                    checkWinner(char1, char2, char3, char4 , char5)
+                if(cells.length>16 && i=== 4){
+                    checkWinner(cells.length,char1, char2, char3, char4 , char5)
                         }
         }
     });
